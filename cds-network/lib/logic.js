bf1243ccfad73fa96c3a88699cb160ff26d2ac62
Edison <edison.limjh@gmail.com>
2018-03-08 01:44:11 -0500

    fabric - added payment mechanisms

--- cds-network/lib/logic.js
+++ cds-network/lib/logic.js
@@ -10,37 +10,47 @@ function payOut(paymentReceived) {
     var payOut = payment.amountPaid;
 
     console.log('Received at: ' + paymentReceived.timestamp);
-    console.log('Contract arrivalDateTime: ' + contract.arrivalDateTime);
-
-    // set the status of the payment
-    payment.status = 'ARRIVED';
+    console.log('Contract arrivalDateTime: ' + contract.timestamp);
 
     // if the payment did not arrive on time the payout is zero
     // todo: work on logic to adjust late payments
-    
+    /*
     if (paymentReceived.timestamp > contract.arrivalDateTime) {
         payOut = 0;
         console.log('Late payment');
     }
+    */
 
     console.log('Payout: ' + payOut);
-    contract.CDSBuyer.accountBalance += payOut;
-    contract.BondIssuer.accountBalance -= payOut;
 
-    console.log('CDSBuyer: ' + contract.CDSBuyer.$identifier + ' new balance: ' + contract.CDSBuyer.accountBalance);
-    console.log('BondIssuer: ' + contract.BondIssuer.$identifier + ' new balance: ' + contract.BondIssuer.accountBalance);
+    contract.bondIssuer.accountBalance -= payOut;
+    contract.cdsBuyer.accountBalance += payOut;
+  
+  	// update state after external calls
+  	contract.amountPaid +=payOut;
+  	if (contract.principalAmount - contract.amountPaid <= 0) { 
+    	contract.status = 'FULFILLED';  
+    } else {
+    	contract.status = 'IN_PROGRESS';
+    }
+    // set the status of the payment
+    payment.status = 'IN_PROGRESS';
+
+
+    console.log('CDSBuyer: ' + contract.cdsBuyer.$identifier + ' new balance: ' + contract.cdsBuyer.accountBalance);
+    console.log('BondIssuer: ' + contract.bondIssuer.$identifier + ' new balance: ' + contract.bondIssuer.accountBalance);
 
     return getParticipantRegistry('org.biznet.cds.CDSBuyer')
         .then(function (CDSBuyerRegistry) {
             // update the CDSBuyer's balance
-            return CDSBuyerRegistry.update(contract.CDSBuyer);
+            return CDSBuyerRegistry.update(contract.cdsBuyer);
         })
         .then(function () {
             return getParticipantRegistry('org.biznet.cds.BondIssuer');
         })
         .then(function (BondIssuerRegistry) {
             // update the BondIssuer's balance
-            return BondIssuerRegistry.update(contract.BondIssuer);
+            return BondIssuerRegistry.update(contract.bondIssuer);
         })
         .then(function () {
             return getAssetRegistry('org.biznet.cds.Payment');
@@ -48,5 +58,14 @@ function payOut(paymentReceived) {
         .then(function (paymentRegistry) {
             // update the state of the payment
             return paymentRegistry.update(payment);
-        });
+        })
+  		.then(function () {
+            return getAssetRegistry('org.biznet.cds.Contract');
+        })
+        .then(function (contractRegistry) {
+            // update the state of the payment
+            return contractRegistry.update(contract);
+        })
+  		
+  ;
 }
