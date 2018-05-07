ee9958aace734865d4b17091e5bd2bda385140b7
Edison Lim <edisonlim@Edisons-MacBook-Pro.local>
2018-05-07 02:57:14 -0400

    fixed code that broke after hyperledger composer upgrade

--- cds-network/startComposerFunctions.sh
+++ cds-network/startComposerFunctions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-composer runtime install --card PeerAdmin@hlfv1 --businessNetworkName cds-network
+composer network install --archiveFile cds-network --card PeerAdmin@hlfv1
 composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --archiveFile cds-network@0.0.1.bna --file networkadmin.card
 composer card import --file networkadmin.card
 composer network ping --card admin@cds-network
